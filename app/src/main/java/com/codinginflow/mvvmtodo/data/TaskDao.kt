4ec71267fc7394726eefb84fbf345433610415dd
subhendra-p <subhendra.rpandey@gmail.com>
2020-12-08 23:36:15 +0530

    added hide completed and sort functionality

--- app/src/main/java/com/codinginflow/mvvmtodo/data/TaskDao.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/data/TaskDao.kt
@@ -1,12 +1,22 @@
 package com.codinginflow.mvvmtodo.data
 
 import androidx.room.*
+import com.codinginflow.mvvmtodo.ui.tasks.SortOrder
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TaskDao {
-    @Query("select * from task_table where name like '%' || :searchQuery || '%' order by important desc")
-    fun getTask(searchQuery: String): Flow<List<Task>>
+    fun getTask(searchQuery: String, sortOrder: SortOrder, hideCompleted: Boolean): Flow<List<Task>> =
+            when(sortOrder) {
+                SortOrder.BY_NAME -> getTasksBySortedName(searchQuery, hideCompleted)
+                SortOrder.BY_DATE -> getTasksBySortedDateCreated(searchQuery, hideCompleted)
+            }
+
+    @Query("select * from task_table where (completed != :hideCompleted or completed = 0) and name like '%' || :searchQuery || '%' order by important desc, name")
+    fun getTasksBySortedName(searchQuery: String, hideCompleted : Boolean): Flow<List<Task>>
+
+    @Query("select * from task_table where (completed != :hideCompleted or completed = 0) and name like '%' || :searchQuery || '%' order by important desc, created")
+    fun getTasksBySortedDateCreated(searchQuery: String, hideCompleted : Boolean): Flow<List<Task>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(task: Task)
--- app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksViewModel.kt
@@ -5,14 +5,29 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.codinginflow.mvvmtodo.data.TaskDao
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
 
 class TasksViewModel @ViewModelInject constructor(taskDao: TaskDao) : ViewModel() {
     val searchQuery  = MutableStateFlow("")
 
-    private val taskFlow = searchQuery.flatMapLatest {
-        taskDao.getTask(it)
+//    private val taskFlow = searchQuery.flatMapLatest {
+//        taskDao.getTask(it)
+//    }
+
+    val sortOrder = MutableStateFlow(SortOrder.BY_NAME)
+    val hideCompleted = MutableStateFlow(false)
+
+    private val taskFlow = combine(searchQuery, sortOrder, hideCompleted) {
+searchQuery, sortOrder, hideCompleted ->
+        Triple(searchQuery, sortOrder, hideCompleted)
+    }.flatMapLatest { (searchQuery, sortOrder, hideCompleted) ->
+        taskDao.getTask(searchQuery = searchQuery,sortOrder =  sortOrder, hideCompleted =  hideCompleted)
     }
 
     val tasks = taskFlow.asLiveData()
+}
+
+enum class SortOrder{
+    BY_NAME, BY_DATE
 }
